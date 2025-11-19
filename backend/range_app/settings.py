d8a7788b7f3baaadf6264796f88067f465c682e7
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2025-11-19 11:42:52 +0100

    update CORS allowed origins in settings

--- backend/range_app/settings.py
+++ backend/range_app/settings.py
@@ -93,7 +93,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-cors_origins = os.getenv('CORS_ALLOWED_ORIGINS', 'http://localhost:3000,http://127.0.0.1:3000')
+cors_origins = os.getenv('CORS_ALLOWED_ORIGINS', 'http://localhost:3000,http://127.0.0.1:3000,https://range-teaching-app.vercel.app/')
 CORS_ALLOWED_ORIGINS = [origin.strip() for origin in cors_origins.split(',')]
 
 CORS_ALLOW_CREDENTIALS = True
